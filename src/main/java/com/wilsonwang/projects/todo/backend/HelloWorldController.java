f74a8731e69b7196b7cf10423529f3dc95ab8086
WilsonWang <iloveldt110323@gmail.com>
2021-12-24 18:41:55 +1100

    pathvariable and request parameters

--- src/main/java/com/wilsonwang/projects/todo/backend/HelloWorldController.java
+++ src/main/java/com/wilsonwang/projects/todo/backend/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.wilsonwang.projects.todo.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Controller
 @RestController
@@ -23,5 +20,19 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("hello world");
+    }
+
+    // path variable and request variable
+    // http://localhost:8080/hello-world-bean/ww/name/?id=100&nick=shawn
+    @GetMapping(path = "/hello-world-bean/ww/{pathName}")
+    public HelloWorldBean helloWorldPathVariable(
+            // name defined for RequestParam has to match the request parameter name in the url
+            // otherwise will throw an error
+            // for path variable, you can name whatever you want
+            @PathVariable  String pathName,
+            @RequestParam String id,
+            @RequestParam String nick
+            ) {
+        return new HelloWorldBean("hello world " + pathName + id + nick);
     }
 }
